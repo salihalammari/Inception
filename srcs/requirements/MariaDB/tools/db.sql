bf539b9142035e89f26f7c5e5140099aed15229c
Saliha Lammari <slammari@e2r10p15.1337.ma>
2023-05-29 16:44:38 +0100

    build mariadb

--- srcs/requirements/MariaDB/tools/db.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE $db_name;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$db_root_passwd';
-CREATE USER '$db_user'@'%' IDENTIFIED BY '$db_passwd';
-GRANT ALL PRIVILEGES ON *.* TO '$db_user'@'%';
-FLUSH PRIVILEGES;
-EXIT
-exec 
---
+++ srcs/requirements/MariaDB/tools/script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SQL_DATABASE=salihadb
+SQL_USER=saliha
+SQL_PASSWORD=123
+SQL_ROOT_PASSWORD=321
+
+service mysqld start 
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;" -p$SQL_ROOT_PASSWORD
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+
+mysqld_safe
