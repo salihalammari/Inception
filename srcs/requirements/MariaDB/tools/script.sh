1177b017e87ab942b79928304468e1ffba203099
Saliha Lammari <slammari@e2r10p13.1337.ma>
2023-05-30 22:06:59 +0100

    create wordpress

--- srcs/requirements/MariaDB/tools/script.sh
+++ srcs/requirements/MariaDB/tools/script.sh
@@ -5,7 +5,7 @@ SQL_USER=saliha
 SQL_PASSWORD=123
 SQL_ROOT_PASSWORD=321
 
-service mysqld start 
+service mysql start 
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
---
+++ srcs/requirements/WordPress/tools/script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sed -i -e  's/listen =.*/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+wp core download --path=/var/www/html --allow-root
+wp config create --dbhost=mariadb --dbname=${MYSQL_DB_NAME} --dbuser=${MYSQL_USR} --dbpass=${MYSQL_PWD} --path=/var/www/html --allow-root --skip-check
+wp core install --url=${DOMAINE_NAME} --title="Wordpress page" --admin_name=${WP_USR} --admin_password=${WP_PWD} --admin_email="${ADMIN_EMAIL}"  --path=/var/www/html --allow-root
+wp user create "${USER}" "${USER_EMAIL}" --user_pass=${WP_PWD}  --allow-root --path=/var/www/html
+
+mkdir -p /run/php
+php-fpm7.3 -F
