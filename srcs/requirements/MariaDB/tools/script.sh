994a0c1342023882100e37768a727cbc88124634
Saliha Lammari <slammari@e2r10p13.1337.ma>
2023-06-01 13:06:00 +0100

    mariadb not work

--- srcs/requirements/MariaDB/tools/script.sh
+++ srcs/requirements/MariaDB/tools/script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-mkdir -p /var/www/html
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i 's/#port/port /g' /etc/mysql/mariadb.conf.d/50-server.cnf
 
-service mysql start 
+service mysql start
+sleep 5
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
--- srcs/requirements/WordPress/tools/script.sh
+++ srcs/requirements/WordPress/tools/script.sh
@@ -2,7 +2,11 @@
 
 sed -i -e  's/listen =.*/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 wp core download --path=/var/www/html --allow-root
-wp config create --dbhost=mariadb --dbname=${SQL_DATABASE} --dbuser=${SQL_USER} --dbpass=${SQL_PASSWORD} --path=/var/www/html --allow-root --skip-check
+wp config create --dbhost=mariadb \
+--dbname=${SQL_DATABASE} \
+--dbuser=${SQL_USER} \
+--dbpass=${SQL_PASSWORD} \
+--path=/var/www/html --allow-root
 wp core install --url=${DOMAINE_NAME} --title="Wordpress page" --admin_name=${WP_USR} --admin_password=${WP_PWD} --admin_email="${ADMIN_EMAIL}"  --path=/var/www/html --allow-root
 wp user create "${USER}" "${USER_EMAIL}" --user_pass=${WP_PWD}  --allow-root --path=/var/www/html
 
